4483a25e7a3ba277de529d7d02f399ba985d9ec7
Marcin Szamotulski <mszamot@gmail.com>
2017-06-29 14:25:43 +0200

    Enzyme.ReactWrapper - make all effectful

--- src/Enzyme/ReactWrapper.purs
+++ src/Enzyme/ReactWrapper.purs
@@ -11,15 +11,15 @@ import Unsafe.Coerce (unsafeCoerce)
 
 foreign import data ReactWrapper :: Type
 
-foreign import getNode :: ReactWrapper -> ReactElement
+foreign import getNode :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e)  ReactElement
 
-foreign import getNodes :: ReactWrapper -> Array ReactElement
+foreign import getNodes :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) (Array ReactElement)
 
-foreign import getDOMNode :: ReactWrapper -> HTMLElement
+foreign import getDOMNode :: forall e. ReactWrapper -> Eff (dom :: DOM, enzyme :: ENZYME | e) HTMLElement
 
-foreign import ref :: ReactWrapper -> String -> ReactWrapper
+foreign import ref :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e)  ReactWrapper
 
-foreign import instance_ :: ReactWrapper -> ReactClassInstance
+foreign import instance_ :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) ReactClassInstance
 
 foreign import update :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
@@ -27,133 +27,133 @@ foreign import unmount :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) R
 
 foreign import mount :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
-foreign import setProps :: forall props. ReactWrapper -> props -> ReactWrapper
+foreign import setProps :: forall props e. ReactWrapper -> props -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
 -- todo: setProps with callback
 -- foreign import setPropsFn
 
-foreign import setState :: forall state. ReactWrapper -> state -> ReactWrapper
+foreign import setState :: forall state e. ReactWrapper -> state -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
 -- todo: setState with callback
 -- foreign import setStateFn
 
-foreign import setContext :: forall ctx. ReactWrapper -> ctx -> ReactWrapper
+foreign import setContext :: forall ctx e. ReactWrapper -> ctx -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
-foreign import matchesElement :: ReactWrapper -> ReactElement -> Boolean
+foreign import matchesElement :: forall e. ReactWrapper -> ReactElement -> Eff (enzyme :: ENZYME | e) Boolean
 
-foreign import containsNode :: ReactWrapper -> ReactElement -> Boolean
+foreign import containsNode :: forall e. ReactWrapper -> ReactElement -> Eff (enzyme :: ENZYME | e) Boolean
 
-foreign import containsNodes :: ReactWrapper -> Array ReactElement -> Boolean
+foreign import containsNodes :: forall e. ReactWrapper -> Array ReactElement -> Eff (enzyme :: ENZYME | e) Boolean
 
-foreign import containsMatchingElement :: ReactWrapper -> ReactElement -> Boolean
+foreign import containsMatchingElement :: forall e. ReactWrapper -> ReactElement -> Eff (enzyme :: ENZYME | e) Boolean
 
-foreign import containsAllMatchingElements :: ReactWrapper -> Array ReactElement -> Boolean
+foreign import containsAllMatchingElements :: forall e. ReactWrapper -> Array ReactElement -> Eff (enzyme :: ENZYME | e) Boolean
 
-foreign import containsAnyMatchingElements :: ReactWrapper -> Array ReactElement -> Boolean
+foreign import containsAnyMatchingElements :: forall e. ReactWrapper -> Array ReactElement -> Eff (enzyme :: ENZYME | e) Boolean
 
 -- apriori we don't know what is the cls of the returned ReactWrapper
-foreign import find :: ReactWrapper -> String -> ReactWrapper
+foreign import find :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
-findReactClass :: forall props. ReactWrapper -> ReactClass props -> ReactWrapper 
+findReactClass :: forall props e. ReactWrapper -> ReactClass props -> Eff (enzyme :: ENZYME | e) ReactWrapper 
 findReactClass wrp cls = find wrp (unsafeCoerce cls)
 
 -- todo: find with callback
 -- foreign import findFn
 
-foreign import is :: ReactWrapper -> String -> Boolean
+foreign import is :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) Boolean
 
-foreign import isEmptyRender :: ReactWrapper -> Boolean
+foreign import isEmptyRender :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) Boolean
 
-foreign import filterWhere :: ReactWrapper -> (ReactWrapper -> Boolean) -> ReactWrapper
+foreign import filterWhere :: forall e. ReactWrapper -> (ReactWrapper -> Boolean) -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
-foreign import filter :: ReactWrapper -> String -> ReactWrapper
+foreign import filter :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
 -- todo: filter with callback
 
-foreign import not :: ReactWrapper -> String -> ReactWrapper
+foreign import not :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
 -- todo: not with callback
 
-foreign import text :: ReactWrapper -> String
+foreign import text :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) String
 
-foreign import html :: ReactWrapper -> String
+foreign import html :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) String
 
 -- todo CheerioWrapper
 -- foreign import render :: ReactWrapper -> CheerioWrapper
 
-foreign import simulate :: ReactWrapper -> String -> ReactWrapper
+foreign import simulate :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
-foreign import simulateAndMock :: ReactWrapper -> String -> Foreign -> ReactWrapper
+foreign import simulateAndMock :: forall e. ReactWrapper -> String -> Foreign -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
-foreign import props :: ReactWrapper -> Foreign
+foreign import props :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) Foreign
 
-foreign import state :: ReactWrapper -> String -> Foreign
+foreign import state :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) Foreign
 
-foreign import context :: ReactWrapper -> String -> Foreign
+foreign import context :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) Foreign
 
-foreign import children :: ReactWrapper -> ReactWrapper
+foreign import children :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
-foreign import childrenBySelector :: ReactWrapper -> String -> ReactWrapper
+foreign import childrenBySelector :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
 -- todo: children with callback
 
-foreign import childAt :: ReactWrapper -> Int -> ReactWrapper
+foreign import childAt :: forall e. ReactWrapper -> Int -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
-foreign import parents :: ReactWrapper -> String -> ReactWrapper
+foreign import parents :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
 -- todo: parents with callback
 
-foreign import parent :: ReactWrapper -> ReactWrapper
+foreign import parent :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
-foreign import closest :: ReactWrapper -> String -> ReactWrapper
+foreign import closest :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
 -- todo: closest with callback
 
-foreign import prop :: ReactWrapper -> String -> Foreign
+foreign import prop :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) Foreign
 
-foreign import key :: ReactWrapper -> String
+foreign import key :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) String
 
-foreign import type_ :: ReactWrapper -> String
+foreign import type_ :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) String
 
-foreign import name :: ReactWrapper -> String
+foreign import name :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) String
 
-foreign import hasClass :: ReactWrapper -> String -> Boolean
+foreign import hasClass :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) Boolean
 
 -- foreign import forEach :: ReactWrapper -> (ReactElement -> Unit) -> ReactWrapper
 
-foreign import map :: forall a. ReactWrapper -> (ReactElement -> a) -> Array a
+foreign import map :: forall a e. ReactWrapper -> (ReactElement -> a) -> Eff (enzyme :: ENZYME | e) (Array a)
 
-foreign import reduce :: forall a. ReactWrapper -> (a -> ReactWrapper -> a) -> a -> a
+foreign import reduce :: forall a e. ReactWrapper -> (a -> ReactWrapper -> a) -> a -> Eff (enzyme :: ENZYME | e) a
 
-foreign import reduceRight :: forall a. ReactWrapper -> (a -> ReactWrapper -> a) -> a -> a
+foreign import reduceRight :: forall a e. ReactWrapper -> (a -> ReactWrapper -> a) -> a -> Eff (enzyme :: ENZYME | e) a
 
-foreign import slice :: ReactWrapper -> Int -> Int -> ReactWrapper
+foreign import slice :: forall e. ReactWrapper -> Int -> Int -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
-foreign import some :: ReactWrapper -> String -> Boolean
+foreign import some :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) Boolean
 
 -- todo: some with callback
 
-foreign import someWhere :: ReactWrapper -> (ReactWrapper -> Boolean) -> Boolean
+foreign import someWhere :: forall e. ReactWrapper -> (ReactWrapper -> Boolean) -> Eff (enzyme :: ENZYME | e) Boolean
 
-foreign import every :: ReactWrapper -> String -> Boolean
+foreign import every :: forall e. ReactWrapper -> String -> Eff (enzyme :: ENZYME | e) Boolean
 
 -- todo: every with callback
 
-foreign import everyWhere :: ReactWrapper -> (ReactWrapper -> Boolean) -> Boolean
+foreign import everyWhere :: forall e. ReactWrapper -> (ReactWrapper -> Boolean) -> Eff (enzyme :: ENZYME | e) Boolean
 
-foreign import get :: ReactWrapper -> Int -> ReactElement
+foreign import get :: forall e. ReactWrapper -> Int -> Eff (enzyme :: ENZYME | e) ReactElement
 
-foreign import at :: ReactWrapper -> Int -> ReactWrapper
+foreign import at :: forall e. ReactWrapper -> Int -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
-foreign import first :: ReactWrapper -> ReactWrapper
+foreign import first :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
-foreign import last :: ReactWrapper -> ReactWrapper
+foreign import last :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) ReactWrapper
 
-foreign import isEmpty :: ReactWrapper -> Boolean
+foreign import isEmpty :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) Boolean
 
-foreign import exists :: ReactWrapper -> Boolean
+foreign import exists :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) Boolean
 
-foreign import debug :: ReactWrapper -> String
+foreign import debug :: forall e. ReactWrapper -> Eff (enzyme :: ENZYME | e) String
 
 -- todo: tap
 
